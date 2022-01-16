2f71e1b37ba5afc3ec32dfca6cbb84b3ffba06be
EduuardoPV <eduardoveltroni@hotmail.com>
2022-01-16 17:42:20 -0300

    feat: cria footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,68 @@
+import Image from "next/image";
+
+import {
+  Container,
+  ContainerContent,
+  ContainerImage,
+  ContainerLink,
+  ContainerSocial,
+  Copyright,
+} from "./styles";
+
+import Logo from "../../../public/logo.svg";
+import GooglePlay from "../../../public/components/Footer/googleplay.svg";
+import Facebook from "../../../public/components/Footer/Facebook.svg";
+import Instagram from "../../../public/components/Footer/Instagram.svg";
+
+export function Footer() {
+  return (
+      <Container>
+        <div>
+          <ContainerContent>
+            <ContainerImage>
+              <Image src={Logo} alt="Logo HubLocal" layout="responsive"/>
+
+              <div>
+                <Image src={GooglePlay} alt="Icone Google Play" width={45} height={45} />
+                <Image src={Facebook} alt="Icone Facebook" width={45} height={45}/>
+                <Image src={Instagram} alt="Icone Instagram" width={45} height={45}/>
+              </div>
+            </ContainerImage>
+
+            <ContainerLink>
+              <div>
+                <p>Sobre</p>
+                <ul>
+                  <li>Lorem ipsum</li>
+                  <li>Lorem ipsum</li>
+                  <li>Lorem ipsum</li>
+                  <li>Lorem ipsum</li>
+                  <li>Lorem ipsum</li>
+                </ul>
+              </div>
+              <div>
+                <p>Serviços</p>
+                <ul>
+                  <li>Lorem ipsum</li>
+                  <li>Lorem ipsum</li>
+                  <li>Lorem ipsum</li>
+                  <li>Lorem ipsum</li>
+                </ul>
+              </div>
+              <div>
+                <p>Contato</p>
+                <ul>
+                  <li>Lorem ipsum</li>
+                  <li>Lorem ipsum</li>
+                  <li>Lorem ipsum</li>
+                </ul>
+              </div>
+            </ContainerLink>
+          </ContainerContent>
+
+        </div>
+
+         <span>Copyright @ 2021. HubDev. All Rights Reserved</span>
+      </Container>
+  );
+}
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,95 @@
+import styled from "styled-components";
+
+export const Container = styled.footer`
+  
+  background-color: var(--blue-300);
+  
+  > div {
+    padding: 3rem 5%;
+    max-width: 1240px;
+    margin: 0 auto;
+    
+  }
+
+  span {
+    width: 100%;
+
+    padding: 1.1rem 0;
+    display: block;
+
+    text-align: center;
+    font-size: 0.9rem;
+    color: var(--gray-900);
+    background-color: var(--white);
+  }
+`;
+
+export const ContainerContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 3rem;
+
+  @media (min-width: 1024px) {
+    width: 100%;
+    flex-direction: row;
+    align-items: start;
+    justify-content: space-between;
+  }
+`;
+
+export const ContainerImage = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+
+  cursor: pointer;
+
+  div {
+    flex-wrap: wrap;
+    display: flex;
+    justify-content: center;
+    gap: 2rem;
+  }
+`;
+
+export const ContainerLink = styled.nav`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: start;
+  gap: 2rem;
+  
+  div {
+    min-width: 150px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 0.5rem;
+
+    p {
+      font-size: 1.2rem;
+      color: var(--white);
+      font-weight: bold;
+    }
+
+    ul li {
+      margin-top: 1rem;
+      color: var(--gray-200);
+      opacity: 0.9;
+      cursor: pointer;
+
+        &:hover {
+          opacity: 1;
+          color: var(--green-300);
+        }
+      }
+    }
+  }  
+
+  @media (min-width: 1024px) {
+    align-items: start;
+  }
+`;
