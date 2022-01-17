dd56a7f96949ae8813754e24151fe1ee675444c1
EduuardoPV <eduardoveltroni@hotmail.com>
2022-01-16 22:03:45 -0300

    feat: cria Plans

--- src/components/Plans/index.tsx
+++ src/components/Plans/index.tsx
@@ -1,7 +1,90 @@
-import { } from './styles'
+import Image from "next/image";
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
+import { useEffect, useState } from "react";
 
-export function Plans() {
+import { Container, Title, CardPlan } from "./styles";
+
+interface PlansProps {
+  props: {
+    plans: {
+      plan1: {
+        type: string;
+        price: string;
+        description: string;
+        services: [
+          {
+            iconService: string;
+            typeService: string;
+          }
+        ];
+      };
+    };
+  };
+}
+
+export function Plans({ props }: PlansProps) {
+  const [isWidth, setIsWidth] = useState(0);
+
+  useEffect(() => {
+    setIsWidth(window.innerWidth);
+    window.addEventListener("resize", () => {
+      setIsWidth(window.innerWidth);
+    });
+  });
+
+  let properties = {};
+
+  if (isWidth < 1024) {
+    properties = {
+      indicators: true,
+      duration: 5000,
+      arrows: false,
+      easing: "ease-out",
+      infinite: true,
+    };
+  } else {
+    properties = {
+      autoplay: false,
+      arrows: false,
+      indicators: false,
+      canSwipe: false,
+      slidesToShow: 3,
+    };
+  }
+
   return (
-    <h1>Teste</h1>
-  )
+    <Container>
+      <Title>Planos disponiveis</Title>
+
+      <Slide {...properties}>
+        {Object.values(props.plans).map((plan, index) => (
+          <CardPlan key={index}>
+            <h3>{plan.type}</h3>
+
+            <p>{plan.price}</p>
+
+            <span>{plan.description}</span>
+
+            <ul>
+              {plan.services.map((service, index) => (
+                <li key={index}>
+                  <Image
+                    src={service.iconService}
+                    alt="Icone que contem no plano"
+                    aria-hidden="true"
+                    height={25}
+                    width={25}
+                  />
+                  <p>{service.typeService}</p>
+                </li>
+              ))}
+            </ul>
+
+            <button>Escolher plano</button>
+          </CardPlan>
+        ))}
+      </Slide>
+    </Container>
+  );
 }
--- src/components/Plans/styles.js
+++ src/components/Plans/styles.js
@@ -1,1 +1,84 @@
 import styled from 'styled-components'
+
+
+export const Container = styled.section`
+  max-width: 1240px;
+  margin: 3rem auto;
+  padding: 0 5%;
+
+  .react-slideshow-wrapper.slide {
+    margin: 3rem  0 1rem 0;
+    padding: 1rem 0;
+  }
+`
+
+export const Title = styled.h2`
+  margin-top: 5rem;
+
+  word-wrap: break-word;
+  font-size: 3rem;
+  text-align: center;
+  font-weight: bold;
+  font-family: Montserrat, sans-serif;
+`
+
+export const CardPlan = styled.div`
+  max-width: 260px;
+
+  margin: 0 auto;
+  padding: 1.5rem;
+
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  box-shadow: 0px 0px 5px 1px rgba(0, 0, 0, 0.25);
+
+  p {
+    font-size: 4rem;
+    color: var(--blue-300);
+  }
+
+  span {
+    width: 100%;
+    padding-bottom: 1rem;
+    display: block;
+    font-size: 0.9rem;
+    color: var(--gray-900);
+    border-bottom: 1px solid var(--gray-300);
+  }
+  ul {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+  ul li {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    
+    p {
+      font-size: 1.2rem;
+      color: var(--gray-900)!important;
+    }
+  }
+
+  button {
+    padding: 0.9rem 2rem;
+
+    border-radius: 2rem;
+    border: 2px solid var(--blue-300);
+    background: transparent;
+    
+    font-size: 1.1rem;
+    font-family: Montserrat, sans-serif;
+
+    cursor: pointer;
+    transition: all 0.1s;
+
+    &:hover {
+      color: var(--white);
+      background: var(--green-300);
+    }
+  }
+`
